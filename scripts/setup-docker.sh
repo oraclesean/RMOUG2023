765482b8cc1a624ab5cdd6d4f8cdb33bf1288d8b
oraclesean <oracle.sean@gmail.com>
2023-02-13 21:38:55 +0000

    Update directories

--- scripts/setup-docker.sh
+++ scripts/setup-docker.sh
@@ -3,7 +3,7 @@ export CONTAINER_NAME=LAB
 export STEP=1
 
 echo "Cloning the Oracle container repository" | tee -a /tmp/setup-docker.log
-sudo su - oracle -c "git clone https://github.com/oracle/docker-images /oradata"
+sudo su - oracle -c "git clone https://github.com/oracle/docker-images /oradata/docker-images"
 
 echo "Pulling database installation file" | tee -a /tmp/setup-docker.log
 sudo su - oracle -c "curl https://objectstorage.us-phoenix-1.oraclecloud.com/p/DZ57l6Jr-KLyJzudUb11MYsDL-qBGU_cUNBAQsQvMFEbTrR-EbVoGVFTWQk6L_pM/n/axh0z5q052rw/b/RMOUG2023/o/LINUX.X64_193000_db_home.zip > /oradata/docker-images/OracleDatabase/SingleInstance/dockerfiles/19.3.0/LINUX.X64_193000_db_home.zip"
